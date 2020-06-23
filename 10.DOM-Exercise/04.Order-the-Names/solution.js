900a327efba058a66745a8d1441d83e8a73f761a
kmotovilkov <monyllakk@abv.bg>
2020-06-23 20:38:24 +0300

    update

--- 10.DOM-Exercise/04.Order-the-Names/solution.js
+++ 10.DOM-Exercise/04.Order-the-Names/solution.js
@@ -1,4 +1,33 @@
 function solve() {
+    document.querySelector("button").addEventListener("click", onClick);
+    let list = {};
 
-    //TODO...
+    const items = document.querySelector("ol").querySelectorAll("li");
+    [...items].forEach(e => {
+        if (e.textContent.trim().length === 0) {
+            return;
+        }
+        const char = e.textContent[0].toLocaleUpperCase();
+        list[char] = e.textContent;
+    });
+
+
+    function onClick() {
+        const input = document.querySelector("input");
+        const value = input.value;
+        const char = value[0].toLocaleUpperCase();
+
+
+        if (list.hasOwnProperty(char) === false) {
+            list[char] = value;
+
+        } else {
+            list[char] = list[char] + ", " + value;
+        }
+
+        let index = char.charCodeAt(0) - 65;
+        items[index].textContent = list[char];
+
+        input.value = "";
+    }
 }
