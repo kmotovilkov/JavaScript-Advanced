b3778550445313d1acb9ab4f2165cfaa4e31207d
kmotovilkov <monyllakk@abv.bg>
2020-06-25 22:02:09 +0300

    update

--- 10.DOM-Exercise/07.Number-Convertor/solution.js
+++ 10.DOM-Exercise/07.Number-Convertor/solution.js
@@ -1,4 +1,43 @@
 function solve() {
+    const input = document.getElementById("input");
+    const selectMenuTo = document.getElementById("selectMenuTo");
+    const output = document.getElementById("result");
 
-    //TODO...
+    if (input === null || selectMenuTo === null || output === null) {
+        throw new Error("Missing elements!");
+    }
+    createOptions();
+
+    document.querySelector("#container > button")
+        .addEventListener("click", onClick);
+
+    function createOptions() {
+        const binary = document.createElement("option");
+        binary.textContent = "Binary";
+        binary.value = "binary";
+        selectMenuTo.appendChild(binary);
+        const hexadecimal = document.createElement("option");
+        hexadecimal.textContent = "Hexadecimal";
+        hexadecimal.value = "hexadecimal";
+        selectMenuTo.appendChild(hexadecimal);
+    }
+
+    function onClick() {
+        let num = Number(input.value);
+        let result;
+        if (selectMenuTo.value === "binary") {
+            result = binaryConcertor();
+        } else if (selectMenuTo.value === "hexadecimal") {
+            result = hexadecimalConvertor();
+        }
+        output.value = result;
+
+        function binaryConcertor() {
+            return (num >>> 0).toString(2)
+        }
+
+        function hexadecimalConvertor() {
+            return num.toString(16).toUpperCase();
+        }
+    }
 }
