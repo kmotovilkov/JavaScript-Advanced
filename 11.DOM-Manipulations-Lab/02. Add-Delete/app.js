36515ae19aa90de61cfcfc3be1f39c766c1def29
kmotovilkov <monyllakk@abv.bg>
2020-06-28 20:06:57 +0300

    update

--- 11.DOM-Manipulations-Lab/02. Add-Delete/app.js
+++ 11.DOM-Manipulations-Lab/02. Add-Delete/app.js
@@ -1,3 +1,23 @@
 function addItem() {
-    //TODO...
+
+    let itemList = document.getElementById('items');
+    let inputText = document.getElementById('newText');
+
+    let inputTextValue = inputText.value;
+    if (!inputTextValue) {
+        return;
+    }
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    a.textContent = '[Delete]';
+    a.href = "#";
+    let text = document.createTextNode(inputTextValue);
+    li.appendChild(text);
+    li.appendChild(a);
+    a.addEventListener('click', function (event) {
+        event.preventDefault();
+        event.target.parentElement.remove();
+    });
+    itemList.appendChild(li);
+    inputText.value = "";
 }
