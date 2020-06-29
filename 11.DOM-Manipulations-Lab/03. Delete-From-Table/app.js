8575b535228a9c5078cf591c44ea11e89dd177d9
kmotovilkov <monyllakk@abv.bg>
2020-06-29 19:15:33 +0300

    update

--- 11.DOM-Manipulations-Lab/03. Delete-From-Table/app.js
+++ 11.DOM-Manipulations-Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,25 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+
+    let customersEmail = Array
+        .from(document.querySelectorAll('#customers td:last-child'));
+    let inputEmail = document.querySelector('input[name="email"]');
+    let result = document.getElementById("result");
+
+    result.innerText = "";
+
+    let inputValue = inputEmail.value;
+    if (inputValue === false) {
+        return;
+    }
+
+    let td = customersEmail.find(function (td) {
+        return td.innerText === inputValue;
+    })
+    inputEmail.value = "";
+    if (!td) {
+        result.innerText = "Not found.";
+        return;
+    }
+    td.parentElement.remove();
+    result.innerText = "Deleted.";
 }
