c67b9a67682fd44194fa86a708feab14d4fdad46
kmotovilkov <monyllakk@abv.bg>
2020-07-02 21:40:17 +0300

    update

--- 12.DOM-Manipulations-Exercise/04. Locked-Profile/app.js
+++ 12.DOM-Manipulations-Exercise/04. Locked-Profile/app.js
@@ -1,3 +1,26 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    document.querySelector('main').addEventListener('click', onClick);
+
+
+    function onClick(e) {
+        if (e.target.nodeName === 'BUTTON') {
+            const parent = e.target.parentNode;
+
+            let lock = parent.querySelector('input[type="radio"][value="lock"]');
+
+            if (lock.checked) {
+                return;
+            }
+            const hiddenFields = Array.from(parent.querySelectorAll('div')).filter(div => div.id.includes('HiddenFields'))[0];
+
+            if (hiddenFields.style.display !== 'block') {
+                hiddenFields.style.display = 'block';
+                e.target.textContent = 'Hide it';
+            } else {
+                hiddenFields.style.display = 'none';
+                e.target.textContent = 'Show more';
+            }
+        }
+    }
 }
