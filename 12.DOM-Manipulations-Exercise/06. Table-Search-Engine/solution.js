3e037e72dad2e4a5cb4a613d71ed568df18b21c6
kmotovilkov <monyllakk@abv.bg>
2020-07-02 22:22:53 +0300

    update

--- 12.DOM-Manipulations-Exercise/06. Table-Search-Engine/solution.js
+++ 12.DOM-Manipulations-Exercise/06. Table-Search-Engine/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-   //TODO...
+
+    document.querySelector('#searchBtn').addEventListener('click', onSearch);
+    let input = document.querySelector('#searchField');
+
+    function onSearch(e) {
+        const query = input.value.trim().toLocaleLowerCase();
+        if (query.trim().length !== 0) {
+
+            let tbody = document.querySelector('tbody');
+
+            Array.from(tbody.querySelectorAll('tr')).forEach(r => {
+
+                r.classList.remove('select');
+            });
+
+            Array.from(tbody.querySelectorAll('td')).forEach(d => {
+                if (d.textContent.trim().toLocaleLowerCase().includes(query)) {
+                    d.parentNode.classList.add('select');
+                }
+            });
+
+            input.value = "";
+        }
+
+
+    }
+
 }
