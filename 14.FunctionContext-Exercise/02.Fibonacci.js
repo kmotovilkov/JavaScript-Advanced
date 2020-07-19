a5cf4fbd0caafe8ca18622d59c2a0a78cb974ca1
kmotovilkov <monyllakk@abv.bg>
2020-07-19 15:27:05 +0300

    update

---
+++ 14.FunctionContext-Exercise/02.Fibonacci.js
@@ -0,0 +1,24 @@
+function getFibonator() {
+    let previus = 0;
+    let current = 1;
+
+
+    function fibonator() {
+        let newNumber = previus + current;
+        previus = current;
+        current = newNumber;
+
+        return current;
+    }
+
+    return fibonator;
+}
+
+let fib = getFibonator();
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
