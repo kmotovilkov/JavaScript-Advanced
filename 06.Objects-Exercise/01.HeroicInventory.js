3f1e34dde4d4036ad1090b82f21b333eb483b948
kmotovilkov <monyllakk@abv.bg>
2021-03-05 21:49:55 +0200

    update

--- 06.Objects-Exercise/01.HeroicInventory.js
+++ 06.Objects-Exercise/01.HeroicInventory.js
@@ -1,21 +1,16 @@
 function heroicInventory(arr) {
-
     let heroData = [];
-    for (let i = 0; i < arr.length; i++) {
-        let tokens = arr[i].split(" / ");
-        let name = tokens[0];
-        let level = Number(tokens[1]);
-        let items = [];
-
-        if (tokens.length > 2) {
-            items = tokens[2].split(", ");
+    for (let line of arr) {
+        let [name, level, items] = line.split(" / ");
+        level = Number(level);
+        items = items ? items.split(", ") : [];
+        let hero = {
+            name,
+            level,
+            items
         }
-
-        let hero = {name: name, level: level, items: items};
         heroData.push(hero);
-
     }
-
     console.log(JSON.stringify(heroData));
 }
 
